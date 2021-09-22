a866d2650cfbcc74a2921df45561dc2f3dd1dde9
wiegz15 <61791449+wiegz15@users.noreply.github.com>
2021-09-22 11:47:39 -0400

    Removed locations

--- Ping Comptuers.ps1
+++ Ping Comptuers.ps1
@@ -3,7 +3,7 @@ $names = Get-content "\Location\Of\CSV\List"
 foreach ($name in $names){
 if (Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue){
     Write-Host "$name,up"|
-    Export-CSV "C:\Users\admin.awiegel\Desktop\UP.csv" -append
+    Export-CSV "\Location\To\Save" -append
 }
 else{
     Write-Host "$name,down"|
